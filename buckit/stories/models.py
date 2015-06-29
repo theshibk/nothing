be7391884a876de26634f9720428f8b01ac6bab8
Austin Krieger <stin18x@yahoo.com>
2015-06-28 22:27:03 -0500

    Added a couple of models, just trying some things out

--- buckit/stories/models.py
+++ buckit/stories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Story(models.Model):
+	caption = models.CharField(max_length=200)
+	#comments = models.OneToManyField()
+	description = models.CharField(max_length=100)
+	checked = models.BooleanField(default=False)
+	
+class Comments(models.Model):
+	text = models.CharField(max_length=500)
+	
